45c4e45bc5e6b0d4936ab36b69273039b2bbb33f
hank7444@gmail.com <>
2025-04-11 11:18:50 +0800

    feat: fix some types

--- playground/src/plugins/widgetGroup/types.ts
+++ playground/src/plugins/widgetGroup/types.ts
@@ -14,12 +14,13 @@ export interface WidgetCategoryOption {
 
 export interface WidgetGroupSchema extends Schema {
   widgetGroupId: string; // parent widget group ID, uuid()
-  widgetGroupCompId?: string | undefined; // widget component ID from the widget dropdown menu
+  widgetGroupCompId?: string; // widget component ID from the widget dropdown menu
   widgetGroupType: string; // 'parent' or 'child'
- 
-    //widgetCategory: string | undefined; // widgetCategory key
-    widget: string | undefined; // wiget component ID
- 
+  //widget: string | undefined; // wiget component ID
+  widgetGroupSection: {
+    widgetCategory?: string;
+    widget?: string;
+  }
 }
 
 export interface WidgetGroupCategoryWidgetIds {
